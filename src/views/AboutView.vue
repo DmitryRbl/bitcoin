f3c0a3c713130e6778b263bb8ffc5c92ea475255
dmitryRnb <diman2012.titov@yandex.ru>
2023-01-03 11:59:30 +0300

    feat: +обраборка запроса

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,14 +3,22 @@
     <v-col>
       <h1>Курс биткоина</h1>
     </v-col>
-    <v-col v-for="currency in info" :key="currency.description">
-      <span class="font-weight-bold">
-        {{ currency.description + ':'  }}
-      </span>
-      <span class="font-weight-normal">
-        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
-      </span>
-    </v-col>
+    <section v-if="errored">
+      <p>Упс.. Не удалось получить данные</p>
+    </section>
+    <section v-else>
+      <div v-if="loading">Получение данных...</div>
+      <div v-else>
+        <v-col v-for="currency in info" :key="currency.description">
+          <span class="font-weight-bold">
+            {{ currency.description + ':'  }}
+          </span>
+          <span class="font-weight-normal">
+            <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+          </span>
+        </v-col>
+      </div>
+    </section>
     <v-btn depressed @click="updateCurse">
       Обновить курс
     </v-btn>
@@ -22,7 +30,9 @@ import axios from 'axios'
 
 export default {
   data: () => ({
-    info: null
+    info: null,
+    loading: true,
+    errored: false
   }),
   filters: {
     currencydecimal(value) {
@@ -31,15 +41,29 @@ export default {
   },
   methods: {
     updateCurse() {
+      this.loading = true;
+      this.errored = false;
       axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response.data.bpi));
+      .then(response => (this.info = response.data.bpi))
+      .catch(error => 
+      { 
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
     }
   },
   mounted() {
     axios
       .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = response.data.bpi));
+      .then(response => (this.info = response.data.bpi))
+      .catch(error => 
+      { 
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
 }
 };
 </script>
